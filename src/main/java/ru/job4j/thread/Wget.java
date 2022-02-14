027705fbeda2c2d1648928b9b4b6f25ad5ffa4af
Evgeny Koregin <mnemonic78@yandex.ru>
2022-02-14 13:05:48 +0300

    4. Скачивание файла с ограничением. [#144271 #263670]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,51 +1,65 @@
 package ru.job4j.thread;
 
-import java.io.*;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Timestamp;
 
 public class Wget implements Runnable {
     private static final int BUFFERSIZE = 1024;
-    private final String url;
+    private static final long ONESECOND = 1000;
+    private final URL url;
     private final int speed;
 
-    public Wget(String url, int speed) {
-        this.url = url;
+    public Wget(String url, int speed) throws MalformedURLException {
+        this.url = new URL(url);
         this.speed = speed;
     }
 
     @Override
     public void run() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("downloaded.file")) {
+        try (BufferedInputStream in = new BufferedInputStream(url.openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(FilenameUtils.getName(url.getPath()))) {
             byte[] dataBuffer = new byte[BUFFERSIZE];
-            int bytesRead = 0;
-            do {
-                Timestamp timeStart = new Timestamp(System.currentTimeMillis());
-                bytesRead = in.read(dataBuffer, 0, BUFFERSIZE);
-                Timestamp timeFinish = new Timestamp(System.currentTimeMillis());
-                long timeDiff = timeFinish.getTime() - timeStart.getTime(); /*Время потраченное на скачивание 1024 байт*/
-                if (timeDiff > 0) {
-                    double currentSpeed = BUFFERSIZE / (timeDiff / 1000.0); /*вычисляем текущую скорость*/
-                    System.out.println("Current speed: " + currentSpeed);
-                    double speedDiff = currentSpeed - (speed); /*Разница скоростей заданной и текущей в мс*/
-                    System.out.println("Разница в скорости: " + speedDiff);
-                    if (speedDiff > 0) {
-                        double pause = (BUFFERSIZE / speedDiff) * 1000; /*Вычисляем паузу задержки*/
-                        System.out.println("Задержка = " + pause);
-                        Thread.sleep((long) pause);
+            int bytesRead;
+            long byteCounter = 0;
+            Timestamp timeStart = new Timestamp(System.currentTimeMillis());
+            Timestamp timeGapStart = new Timestamp(System.currentTimeMillis());
+            while ((bytesRead = in.read(dataBuffer, 0, BUFFERSIZE)) != -1) {
+                byteCounter += bytesRead;
+                if (byteCounter >= speed) {
+                    byteCounter = 0;
+                    long timeSpeedDownload = new Timestamp(System.currentTimeMillis()).getTime() - timeGapStart.getTime();
+                    if (timeSpeedDownload < ONESECOND) {
+                        long pause = ONESECOND - timeSpeedDownload;
+                        System.out.println("Скорость выше указанной. Вносим задержку: " + pause);
+                        Thread.sleep(pause);
                     }
-                    if (bytesRead != -1) {
-                        fileOutputStream.write(dataBuffer, 0, bytesRead);
-                    }
+                    timeGapStart = new Timestamp(System.currentTimeMillis());
                 }
-            } while (bytesRead != -1);
-        } catch (IOException | InterruptedException e) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+            System.out.println("Всего времени на скачивание файла: "
+                    + (new Timestamp(System.currentTimeMillis()).getTime() - timeStart.getTime()) / ONESECOND + " сек");
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void argValidate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Error arguments. Arguments should be: wget <file_url> <download speed: bytes in sec>");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, MalformedURLException {
+        argValidate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
