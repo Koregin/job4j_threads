0cd05f7d3b50efbf6576b74207df87467b164c6b
Evgeny Koregin <mnemonic78@yandex.ru>
2022-02-16 18:39:49 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.ref;
+
+public class User implements Cloneable {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
