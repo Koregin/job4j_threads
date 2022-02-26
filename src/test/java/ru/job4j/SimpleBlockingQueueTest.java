1139a0277f3ee82377c4a47dd3177910e7a19b3d
Evgeny Koregin <mnemonic78@yandex.ru>
2022-02-26 12:38:41 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -3,7 +3,9 @@ package ru.job4j;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
@@ -63,7 +65,6 @@ public class SimpleBlockingQueueTest {
         Thread consumer = new Thread(new Runnable() {
             @Override
             public void run() {
-                Integer temp;
                 for (int i = 1; i < 6; i++) {
                     try {
                         result.add(simpleBlockingQueue.poll());
@@ -78,5 +79,40 @@ public class SimpleBlockingQueueTest {
         producer.join();
         consumer.join();
         assertEquals(List.of(1, 2, 3, 4, 5), result);
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 }
