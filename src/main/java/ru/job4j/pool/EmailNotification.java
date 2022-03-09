8df752d35b3347f45e8c34cd97968bceb61831af
Evgeny Koregin <mnemonic78@yandex.ru>
2022-03-09 16:14:10 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,52 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        String subject =  String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
+        String body = String.format("Add a new event to %s", user.getUsername());
+        send(subject, body, user.getEmail());
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public static void main(String[] args) {
+        EmailNotification emailNotification = new EmailNotification();
+        emailNotification.pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                emailNotification.emailTo(new User("Ivan", "ivan@gmail.com"));
+                System.out.println("Email was sent");
+            }
+        });
+        emailNotification.close();
+    }
+}
+
+class User {
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
