93e0c2122677eff0a9e93d9ed860b5ed7134a72c
Evgeny Koregin <mnemonic78@yandex.ru>
2022-04-02 09:24:47 +0300

    Test commit

---
+++ src/main/java/ru/job4j/git/TestCommit.java
@@ -0,0 +1,9 @@
+package ru.job4j.git;
+
+public class TestCommit {
+    public static void main(String[] args) {
+        System.out.println("Hello test commit 1");
+        System.out.println("Another test commit");
+        System.out.println("Third test commit");
+    }
+}
